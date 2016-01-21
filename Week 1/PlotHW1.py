6a302ec8ac1cdd1be45cccf84ff164733367ba1b
Mr-Damola <damolafowora@gmail.com>
2016-01-20 21:04:36 -0500

    Update PlotHW1.py

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -15,6 +15,25 @@
 
 
 #leave comment here with your information
-#Name:
-#Date:
+#Name:Goodness Fowora
+#Date:January 20th, 2016
+
+import numpy
+import matplotlib.pyplot as plt
+plt.rcdefaults()
+from numpy import sin, cos, pi
+
+x = numpy.linspace(0,6*pi,999999)
+sin_line=sin(x)
+cos_line=cos(x)
+
+plt.gca().set_color_cycle(['blue','red'])
+plt.plot(x, cos_line)
+plt.plot(x, sin_line, '--', linewidth=2)
+plt.ylabel("Y-Axis")
+plt.xlabel("Angles")
+plt.legend(('cos(x)','sin(x)'))
+plt.title('Plot of sin(x) and cos(x) from 0 to 6pi')
+
+plt.show()
 
